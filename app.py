4f418c1f96790e386ceffe3f6180c2bbba9647de
t-anya <tanya.sh0610@gmail.com>
2023-04-23 23:59:08 -0500

    changes in app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+import json
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Login Page"
+
+@app.route('/edit_bank')
+def edit_bank():
+    #todo
+    return render_template('edit_bank_details.html')
+
+@app.route('/edit_ssn')
+def edit_ssn():
+    #todo
+    return render_template('edit_ssn_details.html')
+
+@app.route('/edit_all')
+def edit_all():
+    #todo
+    return render_template('edit_all_details.html')
