c6c1760488ffb6cee25fcbc3cabbc7250bf4d7e1
ka1iht <thilak746@gmail.com>
2023-04-26 23:37:26 -0500

    updated decrypt

--- decrypt.py
+++ decrypt.py
@@ -4,39 +4,46 @@ from Crypto.Protocol.KDF import scrypt
 
 BUFFER_SIZE = 1024 * 1024
 
-user_info = ["Chase", "jxk10000", "SuperSecRetPassWord"]
+def decryptFromFile(userInfo):
+    inFile = open(userInfo[1] + ".encrypted", 'rb')
+    # outFile = open("decrypted.txt", 'wb')
 
-in_file = open(user_info[1] + ".encrypted", 'rb')
-out_file = open("decrypted.txt", 'wb')
+    salt = inFile.read(32)
+    key = scrypt(userInfo[2], salt, key_len = 32, N = 2**18, r = 8, p = 1)
 
-salt = in_file.read(32)
-key = scrypt(user_info[2], salt, key_len = 32, N = 2**18, r = 8, p = 1)
+    nonce = inFile.read(16)
+    cipher = AES.new(key, AES.MODE_GCM, nonce = nonce)
 
-nonce = in_file.read(16)
-cipher = AES.new(key, AES.MODE_GCM, nonce = nonce)
+    inFileSize = os.path.getsize(userInfo[1] + ".encrypted")
+    encryptedDataSize = inFileSize - 32 - 16 - 16
 
-in_file_size = os.path.getsize(user_info[1] + ".encrypted")
-encrypted_data_size = in_file_size - 32 - 16 - 16
+    for _ in range(int(encryptedDataSize / BUFFER_SIZE)):
+        data = inFile.read(BUFFER_SIZE)
+        decryptedData = cipher.decrypt(data)
+        # outFile.write(decryptedData)
 
-for _ in range(int(encrypted_data_size / BUFFER_SIZE)):
-    data = in_file.read(BUFFER_SIZE)
-    decrypted_data = cipher.decrypt(data)
-    out_file.write(decrypted_data)
+    data = inFile.read(int(encryptedDataSize % BUFFER_SIZE))
+    decryptedData = cipher.decrypt(data)
+    # outFile.write(decryptedDdata)
 
-data = in_file.read(int(encrypted_data_size % BUFFER_SIZE))
-decrypted_data = cipher.decrypt(data)
-out_file.write(decrypted_data)
+    tag = inFile.read(16)
+    
+    print(decryptedData)
 
-tag = in_file.read(16)
+    try:
+        cipher.verify(tag)
 
-try:
-    cipher.verify(tag)
+    except ValueError as e:
+        inFile.close()
+        # out_file.close()
+        # os.remove("decrypted.txt")
+        raise e
 
-except ValueError as e:
-    in_file.close()
-    out_file.close()
-    os.remove("decrypted.txt")
-    raise e
+    inFile.close()
+    # out_file.close()
 
-in_file.close()
-out_file.close()
+# Usage
+# from decrypt import decryptFromFile
+
+uinfo = ["Chase", "jxk10000", "SuperSecRetPassWord"]
+decryptFromFile(uinfo)
