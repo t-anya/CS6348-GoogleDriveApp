998f46e09e624f76b0cce5f4c81bdb10bb67ded5
t-anya <tanya.sh0610@gmail.com>
2023-04-29 13:35:57 -0500

    edit bank details

--- app.py
+++ app.py
@@ -20,37 +20,35 @@ def home():
 
 @app.route('/edit_bank')
 def edit_bank():
-    #todo
+    #todo - pwd check
+    pwd = "SuperSecRetPassWord"
+    uinfo = [session["user_name"],session["net_id"],pwd]
 
-    # 1. check whether file exists or not? - after steve code
-
-    # 2. if exists - check modification
-
-    # 3. error -> alert show
-
-    # 4. file decrypt - 
-        # read values
-        # show in ui
-        # and on submit click - again call encrypt
-
-    
-    if (True):
-        print("w")
-
+    decryptedVal = decryptAll(uinfo)
+    if decryptedVal == 0 :
+        #todo -add flash support
+        flash("File is modified", "info")
+        return redirect('/')
     else: 
-        flash("File has been modified!", "info")
-
-
-
-    
-  
-    
-    return render_template('edit_bank_details.html')
+        session["edit_all_dec_val_bank"] = decryptedVal[0]
+        session["edit_all_dec_val_ssn"] = decryptedVal[1]
+        return render_template('edit_bank_details.html')
 
 @app.route('/edit_ssn')
 def edit_ssn():
-    #todo
-    return render_template('edit_ssn_details.html')
+    #todo - pwd check
+    pwd = "SuperSecRetPassWord"
+    uinfo = [session["user_name"],session["net_id"],pwd]
+
+    decryptedVal = decryptAll(uinfo)
+    if decryptedVal == 0 :
+        #todo -add flash support
+        flash("File is modified", "info")
+        return redirect('/')
+    else: 
+        session["edit_all_dec_val_bank"] = decryptedVal[0]
+        session["edit_all_dec_val_ssn"] = decryptedVal[1]
+        return render_template('edit_ssn_details.html')
 
 @app.route('/edit_all')
 def edit_all():
@@ -75,6 +73,38 @@ def write_to_file():
         bank_acc = request.form['bankacc']
         ssn = request.form['ssn']
         bdetails = [bank_acc,ssn]
+
+        pwd = "SuperSecRetPassWord"
+        uinfo = [session["user_name"],session["net_id"],pwd]
+        encryptToFile(uinfo, bdetails)
+
+        flash("File edited!", "info")
+
+
+    return redirect('/')
+
+@app.route('/write_to_file_ssn',methods=['GET', 'POST'])
+def write_to_file_ssn():
+    if request.method == "POST":
+
+        ssn = request.form['ssn']
+        bdetails = [ session["edit_all_dec_val_bank"],ssn]
+
+        pwd = "SuperSecRetPassWord"
+        uinfo = [session["user_name"],session["net_id"],pwd]
+        encryptToFile(uinfo, bdetails)
+
+        flash("File edited!", "info")
+
+
+    return redirect('/')
+
+@app.route('/write_to_file_bank',methods=['GET', 'POST'])
+def write_to_file_bank():
+    if request.method == "POST":
+
+        bank_acc = request.form['bankacc']
+        bdetails = [ bank_acc, session["edit_all_dec_val_ssn"]]
 
         pwd = "SuperSecRetPassWord"
         uinfo = [session["user_name"],session["net_id"],pwd]
